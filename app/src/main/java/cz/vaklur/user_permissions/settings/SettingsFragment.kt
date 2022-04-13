1d00bf56b92ec3f85248c079a5c029d659e962d4
vaklur <bubukaj@gmail.com>
2022-04-13 15:55:31 +0200

    Add SettingsViewModel.kt class

--- app/src/main/java/cz/vaklur/user_permissions/settings/SettingsFragment.kt
+++ app/src/main/java/cz/vaklur/user_permissions/settings/SettingsFragment.kt
@@ -1,17 +1,12 @@
 package cz.vaklur.user_permissions.settings
 
-import android.content.pm.ActivityInfo
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import cz.vaklur.user_permissions.MainActivity
-import cz.vaklur.user_permissions.R
-import cz.vaklur.user_permissions.constants.Constants
+import androidx.lifecycle.ViewModelProvider
 import cz.vaklur.user_permissions.databinding.FragmentSettingsBinding
-import cz.vaklur.user_permissions.volley_communication.CommunicationService
 
 /**
  * Fragment for application settings - server address and language
@@ -21,16 +16,17 @@ class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var communicationService: CommunicationService
-    private lateinit var settingsSP: SettingsSharedPreferences
+    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        communicationService = CommunicationService(requireActivity().application)
-        settingsSP = SettingsSharedPreferences(requireContext())
+        settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+        binding.settingsViewModel = settingsViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        settingsViewModel.getActualLanguage(requireContext())
         return binding.root
     }
 
@@ -39,13 +35,9 @@ class SettingsFragment : Fragment() {
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val dialogs = SettingsDialog(requireActivity().application)
-        // Load and display last known settings
-        binding.languageTV.text = getActualLanguage(settingsSP.getLanguageSettings())
-        binding.actualIPTV.text = settingsSP.getIpSettings()
         // Test connection to server
-        getServerState()
+        settingsViewModel.getServerState(requireActivity(),requireContext())
         // Initialize onClickListeners for buttons
         binding.changeIPBTN.setOnClickListener {
             dialogs.showSetAddressDialog(binding, requireContext())
@@ -54,74 +46,8 @@ class SettingsFragment : Fragment() {
             dialogs.showSetLanguageDialog(requireActivity(), requireContext())
         }
         binding.refreshIPBTN.setOnClickListener {
-            getServerState()
+            settingsViewModel.getServerState(requireActivity(),requireContext())
         }
-    }
-
-    /**
-     * Function set the widget parameters if the test of server connection
-     */
-    private fun setServerStateWidgets(getResponse: Boolean, serverState: Boolean) {
-        binding.changeIPBTN.isEnabled = getResponse
-        binding.refreshIPBTN.isEnabled = getResponse
-        binding.changeLanguageBTN.isEnabled = getResponse
-        (activity as MainActivity).allowBackPressed = getResponse
-
-        val stateActualTV = binding.stateActualTV
-        stateActualTV.text = requireContext().resources.getString(R.string.state_connecting_setting)
-        stateActualTV.setBackgroundColor(Color.TRANSPARENT)
-        var visibility = View.VISIBLE
-        if (getResponse) {
-            visibility = View.GONE
-            requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
-            if (serverState) {
-                stateActualTV.text =
-                    requireContext().resources.getString(R.string.state_reachable_setting)
-                stateActualTV.setBackgroundColor(Color.GREEN)
-            } else {
-                stateActualTV.text = resources.getString(R.string.state_unreachable_setting)
-                stateActualTV.setBackgroundColor(Color.RED)
-            }
-        } else {
-            requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
-        }
-        binding.settingsPB.visibility = visibility
-        binding.settingsProgressBarTV.visibility = visibility
-    }
-
-    /**
-     * Function that returns the full name of the current language, depending on the local language code.
-     *
-     * @param languageCode Code of actual language.
-     *
-     * @return Full name of actual language.
-     */
-    private fun getActualLanguage(languageCode: String): String {
-        var actualLanguage: String = getString(R.string.language_english)
-        if (languageCode == Constants.ENGLISH_CODE) {
-            actualLanguage = getString(R.string.language_english)
-        } else if (languageCode == Constants.CZECH_CODE) {
-            actualLanguage = getString(R.string.language_czech)
-        }
-        return actualLanguage
-    }
-
-    /**
-     * Function for get actual server state and set the widgets based on server state
-     */
-    private fun getServerState() {
-        setServerStateWidgets(getResponse = false, serverState = false)
-        communicationService.testConnectionToServer(
-            settingsSP.getIpSettings(),
-            object : CommunicationService.VolleyStringResponse {
-                override fun onSuccess() {
-                    setServerStateWidgets(getResponse = true, serverState = true)
-                }
-
-                override fun onError() {
-                    setServerStateWidgets(getResponse = true, serverState = false)
-                }
-            })
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/cz/vaklur/user_permissions/settings/SettingsViewModel.kt
@@ -0,0 +1,114 @@
+package cz.vaklur.user_permissions.settings
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import cz.vaklur.user_permissions.MainActivity
+import cz.vaklur.user_permissions.R
+import cz.vaklur.user_permissions.constants.Constants
+import cz.vaklur.user_permissions.volley_communication.CommunicationService
+
+class SettingsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var _serverState = MutableLiveData<String>()
+    val serverState: LiveData<String> = _serverState
+
+    private var _serverStateColor = MutableLiveData<Drawable>()
+    val serverStateColor: LiveData<Drawable> = _serverStateColor
+
+    private var _changeServerAddressButtonEnable = MutableLiveData<Boolean>()
+    val changeServerAddressButtonEnable: LiveData<Boolean> = _changeServerAddressButtonEnable
+
+    private var _refreshServerStateButtonEnable = MutableLiveData<Boolean>()
+    val refreshServerStateButtonEnable: LiveData<Boolean> = _refreshServerStateButtonEnable
+
+    private var _changeLanguageButtonEnable = MutableLiveData<Boolean>()
+    val changeLanguageButtonEnable: LiveData<Boolean> = _changeLanguageButtonEnable
+
+    private var _progressBarVisibility = MutableLiveData<Int>()
+    var progressBarVisibility: LiveData<Int> = _progressBarVisibility
+
+    private var _progressBarTextViewVisibility = MutableLiveData<Int>()
+    var progressBarTextViewVisibility: LiveData<Int> = _progressBarTextViewVisibility
+
+
+    private var settingsSharedPreferences =
+        SettingsSharedPreferences(application.applicationContext)
+    private var communicationService = CommunicationService(application)
+
+    lateinit var actualLanguage:String
+    var actualIpAddress:String
+
+    init {
+        _serverState.value =
+            application.applicationContext.getString(R.string.state_connecting_setting)
+        _serverStateColor.value =
+            AppCompatResources.getDrawable(application.applicationContext, R.color.gray)
+        _changeServerAddressButtonEnable.value = true
+        _changeLanguageButtonEnable.value = true
+        _refreshServerStateButtonEnable.value = true
+        _progressBarVisibility.value = View.GONE
+        _progressBarTextViewVisibility.value = View.GONE
+        actualIpAddress = settingsSharedPreferences.getIpSettings()
+    }
+
+    fun getServerState(activity: Activity, context: Context) {
+        (activity as MainActivity).allowBackPressed = false
+        _serverState.value = context.getString(R.string.state_connecting_setting)
+        _serverStateColor.value = AppCompatResources.getDrawable(context, R.color.gray)
+        _changeServerAddressButtonEnable.value = false
+        _changeLanguageButtonEnable.value = false
+        _refreshServerStateButtonEnable.value = false
+        _progressBarVisibility.value = View.VISIBLE
+        _progressBarTextViewVisibility.value = View.VISIBLE
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
+        communicationService.testConnectionToServer(settingsSharedPreferences.getIpSettings(),
+            object : CommunicationService.VolleyStringResponse {
+                override fun onSuccess() {
+                    _serverState.value = context.getString(R.string.state_reachable_setting)
+                    activity.allowBackPressed = true
+                    _serverStateColor.value = AppCompatResources.getDrawable(context, R.color.green)
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+                    _changeServerAddressButtonEnable.value = true
+                    _changeLanguageButtonEnable.value = true
+                    _refreshServerStateButtonEnable.value = true
+                    _progressBarVisibility.value = View.GONE
+                    _progressBarTextViewVisibility.value = View.GONE
+                }
+
+                override fun onError() {
+                    _serverState.value = context.getString(R.string.state_unreachable_setting)
+                    activity.allowBackPressed = true
+                    _serverStateColor.value = AppCompatResources.getDrawable(context, R.color.red)
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+                    _changeServerAddressButtonEnable.value = true
+                    _changeLanguageButtonEnable.value = true
+                    _refreshServerStateButtonEnable.value = true
+                    _progressBarVisibility.value = View.GONE
+                    _progressBarTextViewVisibility.value = View.GONE
+                }
+            })
+    }
+
+    /**
+     * Function that returns the full name of the current language, depending on the local language code.
+     *
+     * @return Full name of actual language.
+     */
+    fun getActualLanguage(context: Context): String {
+        val languageCode = settingsSharedPreferences.getLanguageSettings()
+        if (languageCode == Constants.ENGLISH_CODE) {
+            actualLanguage = context.getString(R.string.language_english)
+        } else if (languageCode == Constants.CZECH_CODE) {
+            actualLanguage = context.getString(R.string.language_czech)
+        }
+        return actualLanguage
+    }
+}
